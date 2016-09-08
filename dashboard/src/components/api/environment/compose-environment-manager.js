e2937fb26a85ac9452112059305ecd57626c5440
Ann Shumilova <ashumilova@codenvy.com>
2016-09-08 14:52:11 +0300

    CHE-1771: define environment managers
    Signed-off-by: Ann Shumilova <ashumilova@codenvy.com>

---
+++ dashboard/src/components/api/environment/compose-environment-manager.js
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2015-2016 Codenvy, S.A.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Codenvy, S.A. - initial API and implementation
+ */
+'use strict';
+
+import {EnvironmentManager} from './environment-manager';
+
+/**
+ * This is the implementation of environment manager that handles the docker compose format.
+ *
+ * Format sample and specific description:
+ * //TODO
+ *
+ */
+export class ComposeEnvironmentManager extends EnvironmentManager {
+
+  constructor() {
+    super();
+    //TODO define compose specific stuff
+  }
+
+  /**
+   * Retrieves the list of machines.
+   *
+   * @param environment environment's configuration
+   * @returns {Array} list of machines defined in environment
+   */
+  getMachines(environment) {
+    return [];
+  }
+
+  /**
+   * Provides the environment configuration based on machines format.
+   *
+   * @param environment origin of the environment to be edited
+   * @param machines the list of machines
+   * @returns environment's configuration
+   */
+  getEnvironment(environment, machines) {
+    return {};
+  }
+}
